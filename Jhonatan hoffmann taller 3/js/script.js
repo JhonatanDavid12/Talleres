0d41b6ae71448bcd8001fdafe0135c0c9a3a3ecd
JhonatanDavid12 <jhonitynhofinx@gmail.com>
2022-03-14 20:36:37 -0500

    Todos los talleres

---
+++ Jhonatan hoffmann taller 3/js/script.js
@@ -0,0 +1,107 @@
+/* (1) let nombre = "Jhonatan";
+let edad = 19;
+alert("El nombre digitado es " + nombre);
+alert("Su edad es " + edad);
+
+let nombre2 = "David";
+let edad2 = 23;
+alert("El nombre digitado es " + nombre2);
+alert("Su edad es " + edad2);
+
+if(edad2 > edad){
+    alert("David es mayor que Jhonatan")
+} */
+
+
+
+/* (2) var notas;
+var promedio;
+var suma = 0;
+var nnotas = parseInt(prompt("Digite la cantidad de notas a promediar"))
+
+for(i = 1; i <= nnotas; i ++){
+    var notas = parseInt(prompt("Digite la nota " + i));
+    suma = suma + notas;
+}
+promedio = suma / nnotas;
+alert("El resultado es "+ promedio); */
+
+
+
+/* (3) var palabra = "dom";
+var palabraSolicitada = prompt("Digite la palabra correcta");
+
+if(palabra == palabraSolicitada){
+    alert("bienvenidos al nuevo club de los magios")
+}
+
+else{
+    alert("No puedes ingresar, no eres un miembro")
+} */
+
+
+
+/*  (4) var palabra = "vacunado";
+var palabraSolicitada = prompt("Digite si es vacunado, o no es vacunado");
+
+if(palabra == palabraSolicitada){
+    alert("Bienvenido")
+}
+
+else{
+    alert("Por favor ve a vacunarte")
+} */
+
+
+
+/* (5) const fechaNacimiento = document.getElementById(fechaNacimiento).value
+const edad = document.getElementById(edad).value
+
+}
+
+window.addEventListener('load',function(){
+    fechaNacimiento.addEventListener('change',function(){
+        if(this.value){
+        }
+            
+    });
+}); */
+
+
+
+(6) let capital = parseInt(prompt("Digite el pais para saber la capital: \n - 1.COLOMBIA \n - 2.ARGENTINA \n - 3.PERÚ \n - 4.JAPÓN \n - 5.ALEMANIA"));
+
+switch(capital){
+    case 1:
+    alert("Bogotá");
+    break;
+
+    case 2:
+    alert("Buenos Aires");
+    break;
+
+    case 3:
+    alert("Lima");
+    break;
+
+    case 4:
+    alert("Tokio");
+    break;
+
+    case 5:
+    alert("Berlín");
+    break;
+
+    default:
+    alert("No escribiste ningun número");
+        
+}*/
+
+
+j
+/* (7) var tabla = prompt("Que tabla quieres ver")
+
+for(var i = 1; i < 11; i++){
+    document.write(tabla + " x " + i + " = " + (tabla * i + "<br>"))
+} */
+
